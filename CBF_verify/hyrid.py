b04ae6b653987d8faaf8af321f0c7afb9c760a26
tcy1998 <chuyuan2@illinois.edu>
2021-11-29 02:24:53 -0600

    20211128

---
+++ CBF_verify/hyrid.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+Theta, Theta_d, Theta_dd = [], [], []
+T, dt = 1, 0.002
+gamma, alpha = 0.5, np.pi/8
+theta, theta_d, theta_dd = alpha, 0.0, 0.0
+g, l = 9.8, 1.0
+
+for t in range(int(T/dt)):
+    if theta >= gamma + alpha:
+        theta_d = theta_d * np.cos(2*alpha)
+        theta = theta_d * dt + gamma -alpha
+        Theta.append(theta)
+        Theta_d.append(theta_d)
+        # print("hit")
+    else:
+        theta_d += g/l * np.sin(theta) * dt
+        theta += theta_d * dt
+        Theta.append(theta)
+        Theta_d.append(theta_d)
+    t += dt
+
+t_linspace = np.linspace(0, T, num=int(T/dt))
+plt.plot(Theta, Theta_d)
+plt.axvline(gamma+alpha)
+plt.axvline(gamma-alpha)
+plt.show()
