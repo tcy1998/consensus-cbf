1fb3856f8971358ef76dbfc34ee1214b3efb7f7a
tcy1998 <tcy981025@gmail.com>
2023-05-21 16:47:06 -0500

    20230521

--- proposal_mpc_plot.py
+++ proposal_mpc_plot.py
@@ -8,40 +8,159 @@ sys.path.insert(0, os.path.dirname(os.getcwd()))
 import alphashape
 import pickle
 
-with open('LOG_vel_2.pkl', 'rb') as f:
+def distance_to_goal(position, goal):
+    return np.linalg.norm(position - goal)
+
+### Load Velocity Data ###
+with open('LOG_vel_5.pkl', 'rb') as f:
     LOG_vel = pickle.load(f)
 
+scalar = 0.08
 success_point, failed_point = [], []
 for ii in range(len(LOG_vel)):
-    if LOG_vel[ii][0] == 1:
-        success_point.append([LOG_vel[ii][1]*0.08-3, LOG_vel[ii][2]*0.08+1])
+    if LOG_vel[ii][0] == 1 and LOG_vel[ii][1] > -1:         # success velocity
+        success_point.append([LOG_vel[ii][1]*scalar-3, LOG_vel[ii][2]*scalar+1])
     else:
-        failed_point.append([LOG_vel[ii][1]*0.08-3, LOG_vel[ii][2]*0.08+1])
+        failed_point.append([LOG_vel[ii][1]*scalar-3, LOG_vel[ii][2]*scalar+1])
 fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(aspect="equal"))
-ax.scatter(*zip(*success_point), c='b', s=10)
-ax.scatter(*zip(*failed_point), c='r', s=1)
-# plt.show()
+ax.scatter(*zip(*success_point), c='b', s=2)
+# ax.scatter(*zip(*failed_point), c='r', s=1)
 
-# alpha_shape = alphashape.alphashape(points_2d, 0.)
-# fig, ax = plt.subplots()
-# ax.scatter(*zip(*points_2d))                            # plot points
-# ax.add_patch(PolygonPatch(alpha_shape, alpha=0.2))      # plot alpha shape
-# plt.show()
-
-# alpha_shape = alphashape.alphashape(points_2d, 2.0)
-# fig, ax = plt.subplots()
-# ax.scatter(*zip(*points_2d))
-# ax.add_patch(PolygonPatch(alpha_shape, alpha=0.2))
-# plt.show()
-
-x = np.arange(-4,1,0.01)
+### Print Obstacles ###
+x = np.arange(-3.5,0.5,0.01)
 y = np.sin(0.5 * np.pi * x) +1
-plt.axis([-4.0, 1.0, -2.0, 3.0])
+plt.axis([-3.5, 0.5, -1.8, 2.4])
 plt.plot(x, y, color='k', linewidth=1.2)
 plt.plot(x, y-1.5, color='k', linewidth=1.2)
 plt.fill_between(x, 3, y, color='whitesmoke')
 plt.fill_between(x, y-1.5, -2.0, color='whitesmoke')
 
+### Print Test Set Box ###
+xx = np.arange(-2.5*scalar-3, -3+2.5*scalar, 0.01)
+y1 = 1+2.5*scalar + 0*xx
+y2 = 1-2.5*scalar + 0*xx
+plt.plot(xx, y1, color='grey', linewidth=2.2)
+plt.plot(xx, y2, color='grey', linewidth=2.2)
 
+yy = np.arange(1-2.5*scalar, 1+2.5*scalar, 0.01)
+x1 = -3-2.5*scalar + 0*yy
+x2 = -3+2.5*scalar + 0*yy
+plt.plot(x1, yy, color='grey', linewidth=2.2)
+plt.plot(x2, yy, color='grey', linewidth=2.2)
+
+### Target and Start Point ###
+target = (0, 0)
+start = (-3, 1)
+plt.scatter(start[0], start[1], color='green', marker='o', s=40)
+target_circle = plt.Circle((target[0], target[1]), 0.1, color='b', fill=False)
+ax.add_artist(target_circle)
+
+### Load Trajectory Data ###
+with open('LOG_traj_5.pkl', 'rb') as f:
+    LOG_traj = pickle.load(f)
+print(len(LOG_traj))
+
+
+### Print Trajectory ###
+for i in range(len(LOG_traj)):
+    if len(LOG_traj[i][0]) > 0:
+        # print(LOG_traj[i])
+        traj_end_x = LOG_traj[i][0][-1]
+        traj_end_y = LOG_traj[i][1][-1]
+        traj_start_x = LOG_traj[i][0][0]
+        if traj_end_x ** 2 + traj_end_y ** 2 < 0.1 ** 2:
+            plt.plot(LOG_traj[i][0], LOG_traj[i][1], 'b-', linewidth=0.02)
+        # else:
+        #     plt.plot(LOG_traj[i][0], LOG_traj[i][1], 'r-', linewidth=0.05)
+    # print(len(LOG_traj[i]))
+    # plt.plot(x_log, y_log, 'r-')
+
+plt.xlabel('x [m]')
+plt.ylabel('y [m]')
+
+plt.show()
+
+
+### Load Velocity Data ###
+with open('LOG_vel_4.pkl', 'rb') as f:
+    LOG_vel = pickle.load(f)
+
+success_point, failed_point = [], []
+for ii in range(len(LOG_vel)):
+    if LOG_vel[ii][0] == 1:
+        success_point.append([LOG_vel[ii][1]*0.1-3, LOG_vel[ii][2]*0.1+1])
+    else:
+        failed_point.append([LOG_vel[ii][1]*0.1-3, LOG_vel[ii][2]*0.1+1])
+fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(aspect="equal"))
+ax.scatter(*zip(*success_point), c='b', s=2)
+# ax.scatter(*zip(*failed_point), c='r', s=4)
+
+### Print Test Set Box ###
+xx = np.arange(-2.5*0.1-3, -3+2.5*0.1, 0.01)
+y1 = 1+2.5*0.1 + 0*xx
+y2 = 1-2.5*0.1 + 0*xx
+plt.plot(xx, y1, color='grey', linewidth=2.2)
+plt.plot(xx, y2, color='grey', linewidth=2.2)
+
+yy = np.arange(1-2.5*0.1, 1+2.5*0.1, 0.01)
+x1 = -3-2.5*0.1 + 0*yy
+x2 = -3+2.5*0.1 + 0*yy
+plt.plot(x1, yy, color='grey', linewidth=2.2)
+plt.plot(x2, yy, color='grey', linewidth=2.2)
+
+### Print Obstacle Tube ###
+x = np.arange(-4.0,1.5,0.01)
+y = 1.5 + 0*x
+plt.axis([-4.0, 1.5, -2.4, 2.4])
+plt.plot(x, y, color='k', linewidth=1.2)
+plt.plot(x, y-3, color='k', linewidth=1.2)
+plt.fill_between(x, 3, y, color='whitesmoke')
+plt.fill_between(x, y-3, -3.0, color='whitesmoke')
+
+### Print Target and Initial States ###
+target = (0, 0)
+start = (-3, 1)
+
+# Create a scatter plot with the target and start points
+plt.scatter(start[0], start[1], color='green', marker='o', s=40)
+
+target_circle = plt.Circle((target[0], target[1]), 0.1, color='b', fill=False)
+ax.add_artist(target_circle)
+
+### Print Obstacles ###
+circle_obstacles_1 = {'x': 0.5, 'y': 0.5, 'r': 0.5}
+circle_obstacles_2 = {'x': -0.5, 'y': -0.5, 'r': 0.5}
+circle_obstacles_3 = {'x': -1.0, 'y': 0.8, 'r': 0.5}
+target_circle1 = plt.Circle((circle_obstacles_1['x'], circle_obstacles_1['y']), circle_obstacles_1['r'], color='whitesmoke', fill=True)
+target_circle2 = plt.Circle((circle_obstacles_2['x'], circle_obstacles_2['y']), circle_obstacles_2['r'], color='whitesmoke', fill=True)
+target_circle3 = plt.Circle((circle_obstacles_3['x'], circle_obstacles_3['y']), circle_obstacles_3['r'], color='whitesmoke', fill=True)
+target_circle4 = plt.Circle((circle_obstacles_1['x'], circle_obstacles_1['y']), circle_obstacles_1['r'], color='k', fill=False)
+target_circle5 = plt.Circle((circle_obstacles_2['x'], circle_obstacles_2['y']), circle_obstacles_2['r'], color='k', fill=False)
+target_circle6 = plt.Circle((circle_obstacles_3['x'], circle_obstacles_3['y']), circle_obstacles_3['r'], color='k', fill=False)
+plt.gcf().gca().add_artist(target_circle1)
+plt.gcf().gca().add_artist(target_circle2)
+plt.gcf().gca().add_artist(target_circle3)
+plt.gcf().gca().add_artist(target_circle4)
+plt.gcf().gca().add_artist(target_circle5)
+plt.gcf().gca().add_artist(target_circle6)
+
+### Print Trajectory ###
+with open('LOG_traj_4.pkl', 'rb') as f:
+    LOG_traj = pickle.load(f)
+print(len(LOG_traj))
+for i in range(len(LOG_traj)):
+    if len(LOG_traj[i][0]) > 0:
+        # print(LOG_traj[i])
+        traj_end_x = LOG_traj[i][0][-1]
+        traj_end_y = LOG_traj[i][1][-1]
+        if traj_end_x ** 2 + traj_end_y ** 2 < 0.1 ** 2:
+            plt.plot(LOG_traj[i][0], LOG_traj[i][1], 'b-', linewidth=0.02)
+        # else:
+        #     plt.plot(LOG_traj[i][0], LOG_traj[i][1], 'r-', linewidth=0.05)
+    # print(len(LOG_traj[i]))
+    # plt.plot(x_log, y_log, 'r-')
+
+plt.xlabel('x [m]')
+plt.ylabel('y [m]')
 
 plt.show()
--- robust_CBF/proposal.py
+++ robust_CBF/proposal.py
@@ -1,7 +1,7 @@
 from casadi import *
 import tqdm
 
-N = 10 # number of control intervals
+N = 20 # number of control intervals
 
 
 
@@ -9,6 +9,14 @@ N = 10 # number of control intervals
 f = lambda x,u: vertcat(x[2], x[3], u[0], u[1]) # dx/dt = f(x,u)
 
 dt = 0.05 # length of a control interval
+
+circle_obstacles_1 = {'x': 0.5, 'y': 0.5, 'r': 0.5}
+circle_obstacles_2 = {'x': -0.5, 'y': -0.5, 'r': 0.5}
+circle_obstacles_3 = {'x': -1.0, 'y': 0.8, 'r': 0.5}
+
+def distance_circle_obs(x, y, circle_obstacles):
+    return (x - circle_obstacles['x']) ** 2 + (y - circle_obstacles['y']) ** 2 - circle_obstacles['r'] ** 2
+
 def solver_mpc(x_init, y_init, vx_init, vy_init):
 
     opti = Opti() # Optimization problem
@@ -40,11 +48,20 @@ def solver_mpc(x_init, y_init, vx_init, vy_init):
         x_next = X[:,k] + dt/6*(k1+2*k2+2*k3+k4) 
         opti.subject_to(X[:,k+1]==x_next) # close the gaps
 
-    # ---- path constraints -----------
+    # ---- path constraints 1 -----------
     limit_upper = lambda pos_x: sin(0.5*pi*pos_x) + 1.0
     limit_lower = lambda pos_x: sin(0.5*pi*pos_x) - 0.5
     opti.subject_to(limit_lower(pos_x)<=pos_y)
     opti.subject_to(pos_y<=limit_upper(pos_x))   # track speed limit
+
+    # ---- path constraints 2 --------  
+    # opti.subject_to(pos_y<=1.5)
+    # opti.subject_to(pos_y>=-1.5)
+    # opti.subject_to(distance_circle_obs(pos_x, pos_y, circle_obstacles_1) >= 0.0)
+    # opti.subject_to(distance_circle_obs(pos_x, pos_y, circle_obstacles_2) >= 0.0)
+    # opti.subject_to(distance_circle_obs(pos_x, pos_y, circle_obstacles_3) >= 0.0)
+
+    # ---- input constraints --------
     opti.subject_to(opti.bounded(-10,U,10)) # control is limited
 
     # ---- boundary conditions --------
@@ -52,79 +69,100 @@ def solver_mpc(x_init, y_init, vx_init, vy_init):
     opti.subject_to(pos_y[0]==y_init)   # start at position (0,0)
     opti.subject_to(vel_x[0]==vx_init)
     opti.subject_to(vel_y[0]==vy_init)   # start from stand-still 
-    # opti.subject_to(pos_x[-1]==0) 
-    # opti.subject_to(pos_y[-1]==0)  # finish at position (3,3)
-
-    # # ---- initial values for solver ---
-    # opti.set_initial(U, 1)
-    # opti.set_initial(L, 1)
 
     # ---- solve NLP              ------
     opts = {'ipopt.print_level': 0, 'print_time': 0, 'ipopt.sb': 'yes'}
     
     opti.solver("ipopt", opts) # set numerical backend
+    # opti.solver("ipopt") # set numerical backend
     sol = opti.solve()   # actual solve
 
     return sol.value(pos_x[1]), sol.value(pos_y[1]), sol.value(vel_x[1]), sol.value(vel_y[1])
 
 # ---- post-processing        ------
-import matplotlib.pyplot as plt
-x_0, y_0, vx_0, vy_0 = -3, 1, 1.0, 0
-Epi = 5000
-x_log, y_log = [], [] 
-for i in tqdm.tqdm(range(Epi)):
-    try:
-        x_0, y_0, vx_0, vy_0 = solver_mpc(x_0, y_0, vx_0, vy_0)
-        x_log.append(x_0)
-        y_log.append(y_0)
-        if x_0 ** 2 + y_0 ** 2 < 0.01:
-            break
-    except RuntimeError:
-        break
+# import matplotlib.pyplot as plt
+# x_0, y_0, vx_0, vy_0 = -3, 1, 1.0, -1.0
+# Epi = 500
+# x_log, y_log = [], [] 
+# for i in tqdm.tqdm(range(Epi)):
+#     try:
+#         x_0, y_0, vx_0, vy_0 = solver_mpc(x_0, y_0, vx_0, vy_0)
+#         x_log.append(x_0)
+#         y_log.append(y_0)
+#         if x_0 ** 2 + y_0 ** 2 < 0.01:
+#             break
+#     except RuntimeError:
+#         print('RuntimeError')
+#         break
 
-print(x_0, y_0)
+# print(x_0, y_0)
 
-plt.plot(x_log, y_log, 'r-')
-plt.plot(0,0,'bo')
-plt.xlabel('pos_x')
-plt.ylabel('pos_y')
-plt.axis([-4.0, 4.0, -4.0, 4.0])
+# plt.plot(x_log, y_log, 'r-')
+# plt.plot(0,0,'bo')
+# plt.xlabel('pos_x')
+# plt.ylabel('pos_y')
+# plt.axis([-4.0, 4.0, -4.0, 4.0])
 
-x = np.arange(-4,4,0.01)
-y = np.sin(0.5 * pi * x) +1
-plt.plot(x, y, 'g-', label='upper limit')
-plt.plot(x, y-1.5, 'b-', label='lower limit')
-plt.draw()
-plt.pause(1)
-input("<Hit Enter>")
-plt.close()
-plt.show()
+# x = np.arange(-4,4,0.01)
+# y = np.sin(0.5 * pi * x) +1
+# plt.plot(x, y, 'g-', label='upper limit')
+# plt.plot(x, y-1.5, 'b-', label='lower limit')
+# plt.draw()
+# plt.pause(1)
+# input("<Hit Enter>")
+# plt.close()
 
-# def MPC_diff_init(x_0, y_0, vx_0, vy_0):
-#     Epis = 1000
-#     vx_init = vx_0
-#     vy_init = vy_0
-#     for t in tqdm.tqdm(range(Epis)):
-#         try:
-#             x_0, y_0, vx_0, vy_0 = solver_mpc(x_0, y_0, vx_0, vy_0)
-#             if x_0 ** 2 + y_0 ** 2 < 0.01:
-#                 return [1, vx_init, vy_init]
-#         except RuntimeError:
-#             return [0, vx_init, vy_init]
-#     return [0, vx_init, vy_init]
+# target_circle1 = plt.Circle((circle_obstacles_1['x'], circle_obstacles_1['y']), circle_obstacles_1['r'], color='b', fill=False)
+# target_circle2 = plt.Circle((circle_obstacles_2['x'], circle_obstacles_2['y']), circle_obstacles_2['r'], color='b', fill=False)
+# target_circle3 = plt.Circle((circle_obstacles_3['x'], circle_obstacles_3['y']), circle_obstacles_3['r'], color='b', fill=False)
+# plt.gcf().gca().add_artist(target_circle1)
+# plt.gcf().gca().add_artist(target_circle2)
+# plt.gcf().gca().add_artist(target_circle3)
+# x = np.arange(-4,4,0.01)
+# y = 1.5 + 0*x
+# plt.plot(x, y, 'g-', label='upper limit')
+# plt.plot(x, y-3, 'b-', label='lower limit')
+# plt.draw()
+# plt.pause(1)
+# input("<Hit Enter>")
+# plt.close()
 
-# VX = np.arange(-5, 5, 0.1)
-# VY = np.arange(-5, 5, 0.1)
-# LOG_vel = []
-# ii = 0
-# for vx in VX:
-#     for vy in VY:
-#         print("epsidoe", ii)
-#         LOG_vel.append(MPC_diff_init(-3, 1, vx, vy))
-#         ii += 1
+# plt.show()
 
-# print(LOG_vel)
-# import pickle
+def MPC_diff_init(x_0, y_0, vx_0, vy_0):
+    Epis = 1000
+    vx_init = vx_0
+    vy_init = vy_0
+    x_log, y_log = [], []
+    for t in tqdm.tqdm(range(Epis)):
+        try:
+            x_0, y_0, vx_0, vy_0 = solver_mpc(x_0, y_0, vx_0, vy_0)
+            x_log.append(x_0)
+            y_log.append(y_0)
+            if x_0 ** 2 + y_0 ** 2 < 0.01:
+                return [1, vx_init, vy_init], x_log, y_log
+        except RuntimeError:
+            return [0, vx_init, vy_init], x_log, y_log
+    return [0, vx_init, vy_init], x_log, y_log
 
-# with open('LOG_vel_2.pkl', 'wb') as f:
-#     pickle.dump(LOG_vel, f)
+VX = np.arange(-2.5, 2.5, 0.1)
+VY = np.arange(-2.5, 2.5, 0.1)
+LOG_vel = []
+LOG_traj = []
+ii = 0
+for vx in VX:
+    for vy in VY:
+        print("epsidoe", ii)
+        Data_vel, Data_tarj_x, Data_tarj_y = MPC_diff_init(-3, 1, vx, vy)
+        LOG_vel.append(Data_vel)
+        LOG_traj.append([Data_tarj_x, Data_tarj_y])
+        ii += 1
+
+print(LOG_vel)
+import pickle
+
+with open('LOG_vel_5.pkl', 'wb') as f:
+    pickle.dump(LOG_vel, f)
+
+with open('LOG_traj_5.pkl', 'wb') as f:
+    pickle.dump(LOG_traj, f)
